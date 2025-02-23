04d555fabb39acdb755586258564f2131060c88f
malinskogeng <malinskogeng@hotmail.com>
2025-02-23 22:48:40 +0100

    Navbar og forside

--- backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -13,7 +13,11 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
+<<<<<<< Updated upstream
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d6a71d8742bbf99e8278cb12884aca661157fcc")]
+=======
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b6a63fc2bc9a1593546e5c6216843cb40d3c5d4")]
+>>>>>>> Stashed changes
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,12 +1,42 @@
 import React from "react";
-import "../styles.css"; // Correct relative path
-
+import "../styles.css";
+import "./Home.css";
 
 const Home = () => {
   return (
-    <div className="page-container">
-      <h1>Welcome to the Home Page</h1>
-      <p>This is the homepage of our website.</p>
+    <div className="home-container">
+      <section className="hero-image-section">
+        <img src="/images/2222.jpg" alt="Landskap" className="hero-image" />
+      </section>
+        {/* Hero Section - Text */}
+      <section className="hero-text-section">
+        <div className="hero-content">
+          <h1>Veien til lønnsom bærekraft</h1>
+          <p>
+            Vi hjelper deg å identifisere og prioritere tiltak som gir best
+            resultat for bærekraftig utvikling.
+          </p>
+        </div>
+      </section>
+
+      {/* Services Section */}
+      <section className="services">
+        <div className="service">
+          <img src="/images/lys-icon.png" alt="Idea Icon" />
+          <h3>Identifisere og prioritere</h3>
+          <p>Vi identifiserer, sammenligner, og prioriterer de tiltak som best utnytter dine ressurser, og gir deg et klart konkurransefortrinn i ditt marked.</p>
+        </div>
+        <div className="service">
+          <img src="/images/vann-icon.png" alt="Digital Twin Icon" />
+          <h3>Digital tvilling</h3>
+          <p>Med en digital tvilling (digital prototype) kan du bedre forstå og sammenligne hvordan ulike løsninger vil påvirke og prestere i ulike fremtidsscenarioer. Dette gjør det raskere og enklere å vurdere hvilke tiltak man skal gå videre med i et raskt skiftende landskap.</p>
+        </div>
+        <div className="service">
+          <img src="/images/plante-icon.png" alt="Data Icon" />
+          <h3>Dokumentert datagrunnlag</h3>
+          <p>Vi leverer et databasert beslutningsgrunnlag som dokumenterer bl.a. risiko og verdi assosiert med analyserte tiltak. Dette gjør det enkelt å kommunisere nytten av nødvendige tiltak og investeringer til beslutningstakere og interessenter.</p>
+        </div>
+      </section>
     </div>
   );
 };
