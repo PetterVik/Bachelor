1db7e6b887033ed49245e20f5eeb370202225f4f
HermanHanssen <hermanhanssen97@gmail.com>
2025-03-04 18:15:08 +0100

    Admin

---
+++ Admin-page/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+export default function Navbar() {
+    return (
+      <div style={{ 
+        height: "60px", background: "#28283d", color: "#fff", 
+        display: "flex", alignItems: "center", padding: "0 20px"
+      }}>
+        <h3>Admin Dashboard</h3>
+      </div>
+    );
+  }
+  
