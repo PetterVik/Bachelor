cb170ae56f548cecb25ea028096c5bea4bbe3a51
SirWeekend <petter.viken@hotmail.com>
2025-03-13 13:16:54 +0100

    Fikset databasen slik at den fungerer med frontend

--- backend/Models/Project.cs
+++ backend/Models/Project.cs
@@ -1,10 +1,15 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace PureLogicBackend.Models
 {
     public class Project
     {
         public int Id { get; set; }
-        public string? Title { get; set; } //Makes it possible to have no value
-        public string? Description { get; set; } //Makes it possible to have no value
-        public string? ImageUrl { get; set; } //Makes it possible to have no value
+
+        public string? Title { get; set; } // Kan være null
+        public string? Description { get; set; } // Kan være null
+
+        [Column("image_url")] // Sørger for at det matcher databasen
+        public string? ImageUrl { get; set; } 
     }
 }
