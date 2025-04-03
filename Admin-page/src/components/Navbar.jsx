a2aa21448e7a940e1fea5e7b848417b50080361a
malinskogeng <malinskogeng@hotmail.com>
2025-04-03 15:20:41 +0200

    Prosjekt admin

--- Admin-page/src/components/Navbar.jsx
+++ Admin-page/src/components/Navbar.jsx
@@ -1,12 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "../styles/AdminNavbar.css"; // Update your CSS as needed
+import logo from '../assets/pure-logic-logo.png';  // Importer bildet
+
 
 const Navbar = () => {
   return (
     <nav className="navbar">
       <div className="logo-container">
-        <h1>Pure Logic Admin</h1>
-        <img src="/pure-logic-logo.png" alt="Pure Logic Logo" className="logo" />
+      <Link to="/" className="logo-link">
+          <h1>Pure Logic</h1>
+      </Link>
+          <img src={logo} alt="Pure Logic Logo" className="logo" />
+          <h2>Administrasjon</h2>
       </div>
     </nav>
   );
--- Admin-page/src/pages/AddProject.jsx
+++ Admin-page/src/pages/AddProject.jsx
@@ -1,5 +1,6 @@
 // src/pages/AddProject.jsx
 import React, { useState } from 'react';
+import Navbar from '../components/Navbar';
 import '../styles/AddProject.css';
 
 const AddProject = () => {
@@ -12,7 +13,49 @@ const AddProject = () => {
       visibleOnWebsite: false,
       sections: [{ subtitle: '', text: '' }], // State for multiple sections
     });
+
+     // Forhåndsdefinerte nøkkelord
+    const predefinedKeywords = [
+        'Grønn industri',
+        'Solkraft',
+        'Areal - og eiendomsutvikling',
+        'LCA og klimagass',
+        'Vindkraft',
+        'Vann',
+        'Industriarkitektur',
+    ];
   
+     // Legge til et nøkkelord
+    const handleKeywordChange = (e) => {
+        const selectedKeyword = e.target.value;
+        if (!formData.keywords.includes(selectedKeyword) && formData.keywords.length < 5) {
+        setFormData({
+            ...formData,
+            keywords: [...formData.keywords, selectedKeyword],
+        });
+        // Nullstiller rullegardinen etter valg
+        e.target.value = ''; // Setter verdien til tomt igjen
+        }
+    };
+
+    // Legge til eget nøkkelord
+    const handleCustomKeywordChange = (e) => {
+        const customKeyword = e.target.value;
+        if (customKeyword && !formData.keywords.includes(customKeyword) && formData.keywords.length < 5) {
+        setFormData({
+            ...formData,
+            keywords: [...formData.keywords, customKeyword],
+        });
+        }
+    };
+
+    // Fjerne et nøkkelord
+    const removeKeyword = (keyword) => {
+        setFormData({
+        ...formData,
+        keywords: formData.keywords.filter((kw) => kw !== keyword),
+        });
+    };
   
    
   // Handle input changes
@@ -56,6 +99,8 @@ const AddProject = () => {
   
   
     return (
+        <>
+        <Navbar />
       <div className="add-project-container">
         <h2>Legg til nytt prosjekt</h2>
         <form onSubmit={handleSubmit}>
@@ -72,7 +117,11 @@ const AddProject = () => {
   
           <div className="form-group">
             <label>Last opp bilde</label>
+            <label htmlFor="file-input" className="custom-file-upload">
+                Bla gjennom datamaskinen her
+            </label>
             <input
+              id="file-input"
               type="file"
               name="image"
               onChange={handleChange}
@@ -82,34 +131,70 @@ const AddProject = () => {
           <div className="form-group">
             <label>Kort beskrivelse</label>
             <textarea
+              type="text"
               name="description"
               value={formData.description}
               onChange={handleChange}
             />
           </div>
   
-          <div className="form-group">
-            <label>Nøkkelord</label>
-            <input
-              type="text"
-              name="keywords"
-              value={formData.keywords}
-              onChange={handleChange}
-            />
-          </div>
+          {/* Nøkkelord */}
+        <div className="form-group">
+          <label>Nøkkelord</label>
+          <select onChange={handleKeywordChange} disabled={formData.keywords.length >= 5}>
+            <option value="" disabled selected>Velg nøkkelord</option>
+            {predefinedKeywords.sort().map((keyword, index) => (
+              <option key={index} value={keyword}>
+                {keyword}
+              </option>
+            ))}
+          </select>
+          <input
+            type="text"
+            placeholder="Nytt nøkkelord"
+            onBlur={handleCustomKeywordChange}
+            maxLength="50"
+            disabled={formData.keywords.length >= 5}
+          />
+        </div>
+        <div>
+          {formData.keywords.length > 0 && (
+            <div>
+              <strong>Valgte nøkkelord:</strong>
+              <ul>
+                {formData.keywords.map((keyword, index) => (
+                  <li key={index}>
+                    {keyword}{' '}
+                    <button type="button" onClick={() => removeKeyword(keyword)}>
+                      x
+                    </button>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          )}
+        </div>
 
           {formData.sections.map((section, index) => (
           <div key={index} className="form-group">
             <label>Underoverskrift</label>
-            <input
-              type="text"
+            <select
               name="subtitle"
               value={section.subtitle}
               onChange={(e) => handleChange(e, index)}
               required
-            />
+            >
+                <option value="" disabled selected>Velg underoverskrift</option>
+                <option value="Intro">Intro</option>
+                <option value="Oppdrag">Oppdrag</option>
+                <option value="Løsning">Løsning</option>
+                <option value="Resultat">Resultat</option>
+            </select>
+
+
             <label>Tekst</label>
             <textarea
+              type="text"
               name="text"
               value={section.text}
               onChange={(e) => handleChange(e, index)}
@@ -123,7 +208,7 @@ const AddProject = () => {
                 className="remove-section-button"
                 onClick={() => removeSection(index)}
               >
-                -
+                x
               </button>
             )}
           </div>
@@ -140,12 +225,40 @@ const AddProject = () => {
         </button>
 
 
+        <div className="form-group">
+        <label>Synlig på nettside:</label>
+            <div>
+                <label>
+                <input 
+                    type="radio" 
+                    name="visibleOnWebsite" 
+                    value="yes" 
+                    checked={formData.visibleOnWebsite === 'yes'} 
+                    onChange={handleChange} 
+                />
+                Ja
+                </label>
+                <label>
+                <input 
+                    type="radio" 
+                    name="visibleOnWebsite" 
+                    value="no" 
+                    checked={formData.visibleOnWebsite === 'no'} 
+                    onChange={handleChange} 
+                />
+                Nei
+                </label>
+            </div>
+        </div>
+
         {/* Submit button */}
         <div>
           <button type="submit" className="submit">Last opp prosjekt</button>
         </div>
+
       </form>
     </div>
+    </>
   );
 };
 
