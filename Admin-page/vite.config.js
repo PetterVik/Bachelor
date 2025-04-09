7601af02e87cb687dfb3a031e47626e9e5ae75d6
SirWeekend <petter.viken@hotmail.com>
2025-04-09 16:13:14 +0200

    Oppdaterte vite.config.js slik at Admin-page skulle kjøre uten feil

--- Admin-page/vite.config.js
+++ Admin-page/vite.config.js
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      warning: path.resolve(__dirname, 'node_modules', 'warning', 'warning.js')
+    }
+  },
   optimizeDeps: {
-    include: ['react-big-calendar']
+    include: ['react-big-calendar', 'warning']
   }
 })
