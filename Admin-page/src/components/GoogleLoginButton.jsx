06b4a8dc0bba98f7d8cf4e05824af4d15bfde99b
HermanHanssen <hermanhanssen97@gmail.com>
2025-04-30 16:08:24 +0200

    Fikset tilbake-knapp

--- Admin-page/src/components/GoogleLoginButton.jsx
+++ Admin-page/src/components/GoogleLoginButton.jsx
@@ -4,7 +4,12 @@ import axios from 'axios';
 import { jwtDecode } from 'jwt-decode';
 
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 const allowedEmails = ['hermanhanssen97@gmail.com', 'petterviken97@gmail.com', 'malinskogeng12@gmail.com']; // eposter som har tilgang
+=======
+
+const allowedEmails = ['hermanhanssen97@gmail.com', 'petterviken97@gmail.com', 'malinskogeng@gmail.com'];
+>>>>>>> Stashed changes
 =======
 
 const allowedEmails = ['hermanhanssen97@gmail.com', 'petterviken97@gmail.com', 'malinskogeng@gmail.com'];
