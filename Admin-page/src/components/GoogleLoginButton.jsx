02f9d25dbbc6185ab777725f98aad483e221a2ae
malinskogeng <malinskogeng@hotmail.com>
2025-05-12 15:12:32 +0200

    login design

--- Admin-page/src/components/GoogleLoginButton.jsx
+++ Admin-page/src/components/GoogleLoginButton.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { useGoogleLogin } from '@react-oauth/google';
 import axios from 'axios';
 import { jwtDecode } from 'jwt-decode';
+import googleLogo from '../assets/google.png'; // Importer Google-logoen
 
 
 const allowedEmails = ['hermanhanssen97@gmail.com', 'petterviken97@gmail.com', 'malinskogeng12@gmail.com']; // eposter som har tilgang
@@ -50,10 +51,39 @@ const GoogleLoginButton = () => {
   });
 
   return (
-    <button onClick={() => login()}>
-      Logg inn med Google
-    </button>
+    <button style={styles.loginButton} onClick={() => login()}>
+    <img src={googleLogo} alt="Google Icon" style={styles.icon} />
+    <span style={styles.buttonText}>Logg inn med Google</span>
+  </button>
   );
+};
+
+const styles = {
+  loginButton: {
+    backgroundColor: 'white',
+    color: '#4285F4', // Google Blue color
+    border: '1px solid #4285F4', // Blue border
+    padding: '12px 25px',
+    fontSize: '1.2rem',
+    fontWeight: 'bold',
+    cursor: 'pointer',
+    borderRadius: '20px', // Rounded corners
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '100%',
+    maxWidth: '300px',
+    margin: '10px 0',
+    boxShadow: '0px 4px 6px rgba(0, 0, 0, 0.1)', // Soft shadow for depth
+  },
+  icon: {
+    width: '20px',
+    height: '20px',
+    marginRight: '10px', // Space between icon and text
+  },
+  buttonText: {
+    color: '#4285F4', // Google blue color for text
+  },
 };
 
 export default GoogleLoginButton;
